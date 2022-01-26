e3d49eb49696af2ace82d609ef08826cd068b604
Grigoriy <ggarnov@mail.ru>
2022-01-26 16:57:28 +0300

    added update user info endpoint

--- src/modules/User/user.interfaces.ts
+++ src/modules/User/user.interfaces.ts
@@ -1,4 +1,5 @@
-import { User } from "../../entities/User"; 
+import { ID } from "../../common/common.interfaces";
+import { Position, Roles, User } from "../../entities/User"; 
 
 export interface CommonResponse {
     statusCode: number;
@@ -16,4 +17,20 @@ export interface RegisterRequestBody {
     workplace: string;
     fullname: string;
     password: string;
+}
+
+export interface UpdateUserRequestBody {
+    token: string;
+    id: ID;
+    email: string;
+    password: string;
+    workplace: string;
+    fullname: string;
+    position: Position;
+    rating: number;
+    role: Roles;
+}
+export type UpdateRepositoryData = UpdateUserRequestBody;  
+export interface UpdateUserResponse extends CommonResponse {
+    user: User;
 }
--- src/modules/User/user.services.ts
+++ src/modules/User/user.services.ts
@@ -1,10 +1,11 @@
 import { JWT } from 'fastify';
 import { getCustomRepository } from 'typeorm';
-import { UserRepository } from './user.repositories';
-import { allErrors } from './user.messages';
 import { DecodedJWT } from '../../common/common.interfaces';
 import { buildError } from '../../utils/error.helper';
 import { User } from '../../entities/User';
+import { UserRepository } from './user.repositories';
+import { allErrors } from './user.messages';
+import { UpdateUserRequestBody } from './user.interfaces';
 
 export const checkUserService = async (
     token: string,
@@ -18,3 +19,21 @@ export const checkUserService = async (
   
     return user;
   };
+export const updateUserService = async(
+    data: UpdateUserRequestBody,
+    token: string,
+    jwt: JWT
+): Promise<User> => {
+    const decoded: DecodedJWT = jwt.verify(token);
+    if (decoded.isRefresh) throw buildError(400, allErrors.incorectToken);
+    const userRepo = getCustomRepository(UserRepository);
+    const isAUser = await userRepo.findOne({ where: { id: decoded.id } });
+    if (!isAUser) throw buildError(400, allErrors.userNotFound);
+
+    const updateResult = await userRepo.updateUser({ ...data, id: decoded.id });
+    if (!updateResult?.affected) throw buildError(400, allErrors.userNotFound);
+    const user = await userRepo.findOneUserByKey('id', decoded.id);
+    if (!user) throw buildError(400, allErrors.userNotFound);
+
+    return user;
+};  
