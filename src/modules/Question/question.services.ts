f741b73911938d0b1588e751a1e84e49f9ec26f1
Grigoriy <ggarnov@mail.ru>
2022-02-12 11:47:21 +0300

    fix

--- src/modules/Question/question.services.ts
+++ src/modules/Question/question.services.ts
@@ -1,34 +1,17 @@
 import { getCustomRepository } from 'typeorm';
-import { JWT } from 'fastify-jwt';
 import { Question } from '../../entities/Question';
 import {
   AddQuestionRequestBody,
   GetQuestionsRequestBody,
 } from './question.interfaces';
-import { DecodedJWT } from '../../common/common.interfaces';
-import { allErrors } from '../../common/common.messages';
 import { QuestionRepository } from './question.repositories';
-import { checkAdminOrModeratorService } from '../User/user.services';
-import { buildError } from '../../utils/error.helper';
 
 export const addQuestionService = async (
-  data: AddQuestionRequestBody,
-  token: string,
-  jwt: JWT
+  data: AddQuestionRequestBody
 ): Promise<Question> => {
-  const decoded: DecodedJWT = jwt.verify(token);
-  const isAdminOrModerator = await checkAdminOrModeratorService(
-    decoded.id,
-    token,
-    jwt
-  );
-  if (isAdminOrModerator) {
-    const questionRepo = getCustomRepository(QuestionRepository);
-    const question = await questionRepo.createQuestion(data);
-    return question;
-  } else {
-    throw buildError(400, allErrors.tokenNotFound);
-  }
+  const questionRepo = getCustomRepository(QuestionRepository);
+  const question = await questionRepo.createQuestion(data);
+  return question;
 };
 export const getQuestionsService = async (
   data: GetQuestionsRequestBody
