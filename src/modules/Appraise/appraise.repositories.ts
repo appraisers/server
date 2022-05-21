dee2018dcd35d671300b249cebcef8b1dfa43020
KirushaGIT <kirill.garnov@websailors.pro>
2022-05-21 17:01:09 +0300

    improved routes

--- src/modules/Appraise/appraise.repositories.ts
+++ src/modules/Appraise/appraise.repositories.ts
@@ -2,82 +2,89 @@ import { EntityRepository, Repository } from 'typeorm';
 import { Appraise } from '../../entities/Appraise';
 import { APPRAISE_LIMIT, APPRAISE_OFFSET } from './appraise.constants';
 import {
-    CreateAppraiseResponse,
-    GetAppraisesUsersData,
-    SetAppraiseStatusResponse,
+  CreateAppraiseResponse,
+  GetAppraisesUsersData,
+  SetAppraiseStatusResponse,
 } from './appraise.interfaces';
 import { GetAppraiseResponse } from './appraise.interfaces';
 
 @EntityRepository(Appraise)
 export class AppraiseRepository extends Repository<Appraise> {
-    async createAppraise(data: CreateAppraiseResponse): Promise<Appraise> {
-        const { user, author } = data;
-        const appraise = new Appraise();
-        appraise.user = user;
-        appraise.author = author;
-        await this.save(appraise);
-        return appraise;
-    }
-    async setAppraiseStatus(data: SetAppraiseStatusResponse) {
-        const { userId, authorId } = data;
-        return this.createQueryBuilder('appraise')
-            .update(Appraise)
-            .set({ status: true })
-            .where('user_id = :userId', { userId })
-            .andWhere('author_id = :authorId', { authorId })
-            .execute();
-    }
-    async findAppraises(data: GetAppraiseResponse): Promise<Appraise[]> {
-        const {
-            userId,
-            authorId,
-            limit,
-            offset,
-            createdAtAfter,
-            lastMonth,
-            lastYear,
-        } = data;
+  async createAppraise(data: CreateAppraiseResponse): Promise<Appraise> {
+    const { user, author } = data;
+    const appraise = new Appraise();
+    appraise.user = user;
+    appraise.author = author;
+    await this.save(appraise);
+    return appraise;
+  }
+  async setAppraiseStatus(data: SetAppraiseStatusResponse) {
+    const { userId, authorId } = data;
+    return this.createQueryBuilder('appraise')
+      .update(Appraise)
+      .set({ status: true })
+      .where('user_id = :userId', { userId })
+      .andWhere('author_id = :authorId', { authorId })
+      .execute();
+  }
+  async findAppraises(data: GetAppraiseResponse): Promise<Appraise[]> {
+    const {
+      userId,
+      authorId,
+      limit,
+      offset,
+      createdAtAfter,
+      lastMonth,
+      lastYear,
+    } = data;
 
-        //Setting the datetime with the last month from the first day.
-        const lastMonthDate = new Date();
-        lastMonthDate.setDate(1);
+    //Setting the datetime with the last month from the first day.
+    const lastMonthDate = new Date();
+    lastMonthDate.setDate(1);
 
-        //Setting the datetime with the last year from the first month and the first day (example: 2022-01-01).
-        const lastYearDate = new Date();
-        lastYearDate.setMonth(0);
-        lastYearDate.setDate(1);
-        const query = this.createQueryBuilder('appraise').select(['appraise']);
-        if (userId != null) query.andWhere('user_id = :userId', { userId });
-        if (authorId != null) query.andWhere('author_id = :authorId', { authorId });
-        if (createdAtAfter != null) query.andWhere('created_at >= :createdAtAfter', { createdAtAfter });
-        if (lastMonth != null) query.andWhere('created_at >= :lastMonthDate', { lastMonthDate });
-        if (lastYear != null) query.andWhere('created_at >= :lastYearDate', { lastYearDate })
-        query.orderBy('appraise.createdAt', 'ASC')
-            .limit(limit)
-            .offset(offset);
-        return await query.getMany();
-    }
-    async findAppraisesUsers(data: GetAppraisesUsersData): Promise<Appraise[]> {
-        const {
-            userId,
-            authorId,
-            limit,
-            offset,
-        } = data;
+    //Setting the datetime with the last year from the first month and the first day (example: 2022-01-01).
+    const lastYearDate = new Date();
+    lastYearDate.setMonth(0);
+    lastYearDate.setDate(1);
+    const query = this.createQueryBuilder('appraise')
+      .select(['appraise'])
+      .innerJoinAndSelect('appraise.user', 'user')
+      .innerJoinAndSelect('appraise.author', 'author');
+    if (userId != null) query.andWhere('user_id = :userId', { userId });
+    if (authorId != null) query.andWhere('author_id = :authorId', { authorId });
+    if (createdAtAfter != null)
+      query.andWhere('appraise.created_at >= :createdAtAfter', {
+        createdAtAfter,
+      });
+    if (lastMonth != null)
+      query.andWhere('appraise.created_at >= :lastMonthDate', {
+        lastMonthDate,
+      });
+    if (lastYear != null)
+      query.andWhere('appraise.created_at >= :lastYearDate', { lastYearDate });
+    query
+      .orderBy('appraise.createdAt', 'ASC')
+      .offset(offset ? Number(offset) : APPRAISE_OFFSET)
+      .limit(limit ? Number(limit) : APPRAISE_LIMIT);
+    return await query.getMany();
+  }
+  async findAppraisesUsers(data: GetAppraisesUsersData): Promise<Appraise[]> {
+    const { userId, authorId, limit, offset } = data;
 
-        const query = this.createQueryBuilder('appraise').select(['appraise']);
-        if (userId != null) {
-            query.andWhere('user_id = :userId', { userId });
-            query.innerJoinAndSelect('appraise.author', 'author');
-        } else if (authorId != null) {
-            query.andWhere('author_id = :authorId', { authorId });
-            query.innerJoinAndSelect('appraise.user', 'user');
-        } else {
-            query.innerJoinAndSelect('appraise.user', 'user');
-        }
-        query.orderBy('appraise.createdAt', 'ASC')
-            .offset(offset ? Number(offset) : APPRAISE_OFFSET)
-            .limit(limit ? Number(limit) : APPRAISE_LIMIT)
-        return await query.getMany();
+    const query = this.createQueryBuilder('appraise').select(['appraise']);
+    if (userId != null) {
+      query.andWhere('user_id = :userId', { userId });
+      query.innerJoinAndSelect('appraise.author', 'author');
+    } else if (authorId != null) {
+      query.andWhere('author_id = :authorId', { authorId });
+      query.innerJoinAndSelect('appraise.user', 'user');
+    } else {
+      query.innerJoinAndSelect('appraise.user', 'user');
     }
+    query
+      .orderBy('appraise.createdAt', 'ASC')
+      .offset(offset ? Number(offset) : APPRAISE_OFFSET)
+      .limit(limit ? Number(limit) : 100);
+    return await query.getMany();
+  }
 }
--- src/modules/Appraise/appraise.services.ts
+++ src/modules/Appraise/appraise.services.ts
@@ -3,36 +3,45 @@ import { Appraise } from '../../entities/Appraise';
 import { buildError } from '../../utils/error.helper';
 import { allErrors } from '../../common/common.messages';
 import { AppraiseRepository } from './appraise.repositories';
-import { GetAppraiseResponse, GetAppraisesUsersData } from './appraise.interfaces';
+import {
+  GetAppraiseResponse,
+  GetAppraisesUsersData,
+} from './appraise.interfaces';
+import { UserRepository } from '../Auth/auth.repositories';
 
 export const getAppraisesService = async (
-    data: GetAppraiseResponse
+  data: GetAppraiseResponse
 ): Promise<Appraise[]> => {
-    const appraiseRepo = getCustomRepository(AppraiseRepository);
-    const appraise = await appraiseRepo.findAppraises(data);
-    if (!appraise) throw buildError(400, allErrors.appraiseNotFound);
-    return appraise;
+  const appraiseRepo = getCustomRepository(AppraiseRepository);
+  const appraise = await appraiseRepo.findAppraises(data);
+  if (!appraise) throw buildError(400, allErrors.appraiseNotFound);
+  return appraise;
 };
 
 export const getAppraisesUsersService = async (
-    data: GetAppraisesUsersData
+  data: GetAppraisesUsersData
 ): Promise<string[]> => {
-    const appraiseRepo = getCustomRepository(AppraiseRepository);
-    const appraises = await appraiseRepo.findAppraisesUsers(data);
-    if (!appraises) throw buildError(400, allErrors.appraiseNotFound);
-    const names = new Set<string>([]);
-    appraises.forEach(appraise => {
-        if (appraise.author != null) {
-            const name = appraise.author.fullname;
-            if (name != null) {
-                names.add(name);
-            };
-        } else {
-            const name = appraise.user.fullname;
-            if (name != null) {
-                names.add(name);
-            };
-        }
-    });
-    return Array.from(names);
+  const appraiseRepo = getCustomRepository(AppraiseRepository);
+  const appraises = await appraiseRepo.findAppraisesUsers(data);
+  if (!appraises) throw buildError(400, allErrors.appraiseNotFound);
+  const users = [];
+  appraises.forEach((appraise) => {
+    if (data.authorId != null && appraise.author != null) {
+      const isAlreadyUser =
+        users.findIndex((user) => user.id === appraise.author.id) >= 0;
+      if (!isAlreadyUser) {
+        users.push({
+          fullname: appraise.author?.fullname,
+          id: appraise.author.id,
+        });
+      }
+    } else if (appraise.user != null) {
+      const isAlreadyUser =
+        users.findIndex((user) => user.id === appraise.user.id) >= 0;
+      if (!isAlreadyUser) {
+        users.push({ fullname: appraise.user?.fullname, id: appraise.user.id });
+      }
+    }
+  });
+  return users;
 };
